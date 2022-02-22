4fd24e987c2034fdd04764cad1ec9b7128b86ee2
Stéphane Krattinger (McKratt) <stef.krattinger@gmail.com>
2022-02-22 11:31:56 +0100

    switch to jest

--- greetings-ui/src/app/app.module.ts
+++ greetings-ui/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
