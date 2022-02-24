2573ed8d632b13b3ac4712e1849765869f5dbba7
Stéphane Krattinger (McKratt) <stef.krattinger@gmail.com>
2022-02-24 14:46:09 +0100

    ui consumer side

--- greetings-service/greetings-bootstrap/src/test/java/io/sokube/greetings/contract/BaseTestClass.java
+++ greetings-service/greetings-bootstrap/src/test/java/io/sokube/greetings/contract/BaseTestClass.java
@@ -3,10 +3,12 @@ package io.sokube.greetings.contract;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.sokube.greetings.domain.Greeting;
 import io.sokube.greetings.domain.GreetingRepository;
+import io.sokube.greetings.domain.event.EventEmitter;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,15 +16,19 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, KafkaAutoConfiguration.class})
 public class BaseTestClass {
 
     @MockBean
     private GreetingRepository repository;
+
+    @MockBean
+    private EventEmitter eventEmitter;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -33,5 +39,6 @@ public class BaseTestClass {
         RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
         given(repository.find(UUID.fromString("03e805ff-5860-49a6-88bc-a1dcda0dd4b4")))
                 .willReturn(Optional.of(Greeting.of("Christmas").to("Marie").build()));
+        given(repository.put(any(Greeting.class))).willAnswer(call -> call.getArgument(0));
     }
 }
